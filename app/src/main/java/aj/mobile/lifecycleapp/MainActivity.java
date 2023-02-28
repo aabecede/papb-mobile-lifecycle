a339be01b86b169f0130b2c0292471478a8692a2
aabecede <aabdul.jabbar1301@gmail.com>
2023-02-28 11:41:58 +0700

    Pengenalan awal OOP Android

--- app/src/main/java/aj/mobile/lifecycleapp/MainActivity.java
+++ app/src/main/java/aj/mobile/lifecycleapp/MainActivity.java
@@ -1,8 +1,13 @@
 package aj.mobile.lifecycleapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,54 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d("state", "On Create");
+
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d("state", "On Start");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d("state", "On Start");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d("state", "On Restart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d("state", "On Resume");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d("state", "On Resume");
+    }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putString("kelas", "PAPB-A");
+        Log.d("state", "On Save Instance State");
+    }
+
+    @Override
+    protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        Log.d("state", "On Restore");
+        String teksKelas = savedInstanceState.getString("kelas");
+        Log.d("show state", teksKelas);
+        TextView teksHello = (TextView) this.findViewById(R.id.firstTextView);
+        teksHello.setText(teksKelas);
     }
 }
